88c43bcd2f66c971eaef258ea14150aa9178e0a3
Rick <medlin.rick@gmail.com>
2022-11-25 21:14:13 -0800

    submit

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,3 +1,4 @@
+<!-- eslint-disable vue/valid-v-on -->
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
     <div class="container">
@@ -48,7 +49,27 @@ export default {
     },
     methods: {
         submitHandler() {
-            console.log("submitHander called - success")
+            console.log("submitHandler called - success")
+
+            const payload = {
+                email: this.email,
+                password: this.password,
+            }
+
+            const requestOptions = {
+                method: "POST",
+                body: JSON.stringify(payload),
+            }
+
+            fetch("http://localhost:8081/users/login", requestOptions)
+            .then((response) => response.json())
+            .then((data) => {
+                if (data.error) {
+                    console.log("Error:", data.message);
+                } else {
+                    console.log(data);
+                }
+            })
         }
     }
 }

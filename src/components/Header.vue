cd0387cae6fbe04b3f9d415e311bfcab6921cf93
Rick <medlin.rick@gmail.com>
2023-01-01 00:14:30 -0800

    submit

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,28 +1,48 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-<div class="container-fluid">
-  <a class="navbar-brand" href="#">Navbar</a>
-  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-      <li class="nav-item">
-        <router-link class="nav-link active" aria-current="page" to="/">Home</router-link>
-      </li>
-      <li class="nav-item">
-        <router-link v-if="store.token == ''" class="nav-link" to="/login">Login</router-link>
-        <a href="javascript:void(0);" v-else class="nav-link" @click="logout">Logout</a>
-      </li>
-    </ul>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <router-link class="nav-link active" aria-current="page" to="/">Home</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link active" to="/books">Books</router-link>
+        </li>
+        <li v-if="store.token !== ''" class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navBarDropDown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Admin</a>
+          <ul class="dropdown-menu" aria-labelledby="navBarDropDown">
+            <li>
+              <router-link class="dropdown-item" to="/admin/users">Manage Users</router-link>
+            </li>
+            <li>
+              <router-link class="dropdown-item" to="/admin/users/0">Add User</router-link>
+            </li>
+            <li>
+              <router-link class="dropdown-item" to="/admin/books">Manage Books</router-link>
+            </li>
+            <li>
+              <router-link class="dropdown-item" :to="{name:'BookEdit', params: {bookId: 0}}">Add Book</router-link>
+            </li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <router-link v-if="store.token == ''" class="nav-link" to="/login">Login</router-link>
+          <a href="javascript:void(0);" v-else class="nav-link" @click="logout">Logout</a>
+        </li>
+      </ul>
 
-    <span class="navbar-text">
-      {{ store.user.first_name ?? '' }}
-    </span>
+      <span class="navbar-text">
+        {{ store.user.first_name ?? '' }}
+      </span>
 
+    </div>
   </div>
-</div>
 </nav>
 </template>
 
@@ -47,7 +67,7 @@ export default {
         body: JSON.stringify(payload),
       }
 
-      fetch("http://localhost:8081/users/logout", requestOptions)
+      fetch(process.env.VUE_APP_API_URL + "/users/logout", requestOptions)
       .then((response) => response.json())
       .then((response) => {
         if (response.error) {
